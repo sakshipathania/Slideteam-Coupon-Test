a15100c275b6d2fd420c2eb22c578817592972d7
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-10-13 13:20:52 +0530

    Update Email_Sign_up_Correct_Data_2.java

--- src/test/java/SetupClass/StepDefinition/Email_Sign_up_Correct_Data_2.java
+++ src/test/java/SetupClass/StepDefinition/Email_Sign_up_Correct_Data_2.java
@@ -17,7 +17,7 @@ import cucumber.api.java.en.Then;
 public class Email_Sign_up_Correct_Data_2 extends SetupClass {
 	
 	WebDriverWait wait = new WebDriverWait(driver,50);
-
+         JavascriptExecutor js = (JavascriptExecutor) driver;
 	
 	@Given("^user is already on Website Home Page ii$")
 	public void user_is_already_on_Website_Home_Page_ii() throws Throwable {
