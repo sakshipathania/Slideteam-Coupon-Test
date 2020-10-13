7f27cb575cd467c3cdd92b6645306cbdd8544fc9
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-10-13 12:01:39 +0530

    Update CO_Checkout.java

--- src/test/java/SetupClass/StepDefinition/CO_Checkout.java
+++ src/test/java/SetupClass/StepDefinition/CO_Checkout.java
@@ -32,13 +32,53 @@ public class CO_Checkout extends SetupClass {
 		driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
 		log.info("It's opening the website URL");
 		Thread.sleep(1000);
+		try {
+			WebElement logout = driver.findElement(By.xpath("//a[contains(text(),'Sign Out')]"));
+			if (logout.isEnabled()) {
+				logout.click();
+				Thread.sleep(8000);
+				driver.navigate().refresh();
+				Thread.sleep(2000);
+			}
+		} catch (NoSuchElementException Ext) {
+
+		}
+	    Thread.sleep(1000);
+		
+		try {
+			WebElement iframe = driver.findElement(By.id("livechat-full-view"));
+			if(iframe.isDisplayed()) {
+				driver.switchTo().frame(iframe);   
+				 Actions act = new Actions(driver);
+				 act.moveToElement(driver.findElement(By.cssSelector("#title .icon-minimize"))).build().perform();
+				 Thread.sleep(2000);
+					WebElement chat1=driver.findElement(By.cssSelector("#title .icon-minimize"));
+					 Thread.sleep(1000);
+						chat1.click();
+						 Thread.sleep(1000);
+						 driver.switchTo().defaultContent();
+						 Thread.sleep(1000);
+						 driver.switchTo().parentFrame();
+					 Thread.sleep(1000);
+			}
+			else {
+				
+
+			System.out.println("chat window does not open");
+			}
+		}
+				catch(NoSuchElementException NCP) {
+					
+				}
+		Thread.sleep(1000);
+	    
 	    
 	}
 
 	@Then("^user navigates to sign up page CO$")
 	public void user_navigates_to_sign_up_page_CO() throws Throwable {
 		
-		 WebElement login_signup_btn = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".t3-navbar > ul:nth-child(1) > li:nth-child(10) > a:nth-child(1)")));
+		 WebElement login_signup_btn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/header/div/div/nav/div/div[2]/div[2]/div[2]/div/div[2]/ul/li[2]/a/text()")));
 		 Thread.sleep(3000);
 		 login_signup_btn.click();
 		 Thread.sleep(3000);
@@ -104,7 +144,7 @@ public class CO_Checkout extends SetupClass {
 	@Then("^user enter captcha CO$")
 	public void user_enter_captcha_CO() throws Throwable {
 		
-		WebElement captcha = wait.until(ExpectedConditions.elementToBeClickable(By.id("captchtext")));
+		WebElement captcha = wait.until(ExpectedConditions.elementToBeClickable(By.ClassName("captchaTextBox captchaTextBoxpopup form-control")));
 		 Thread.sleep(3000);
 		captcha.sendKeys("Y3Tt6bfwI");
 		 Thread.sleep(3000);
@@ -114,7 +154,7 @@ public class CO_Checkout extends SetupClass {
 	@Then("^user click on register button to complete sign up CO$")
 	public void user_click_on_register_button_to_complete_sign_up_CO() throws Throwable {
 		
-		WebElement register_btn = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".pg-register-button-new")));
+		WebElement register_btn = wait.until(ExpectedConditions.elementToBeClickable(By.ClassName("pg-register-button-new hvr-rectangle-out btn-download")));
 		 Thread.sleep(3000);
 		register_btn.click();
 		 Thread.sleep(5000);
@@ -122,7 +162,7 @@ public class CO_Checkout extends SetupClass {
 		 try
 		 {
 			// Log out
-			 WebElement login_btn = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".t3-navbar > ul:nth-child(1) > li:nth-child(10) > a:nth-child(1)")));
+			 WebElement login_btn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/header/div/div/nav/div/div[2]/div[2]/div[2]/div/div[2]/ul/li[2]/a")));
 			 Thread.sleep(3000);
 			 login_btn.click();
 			 Thread.sleep(3000);
@@ -149,21 +189,21 @@ public class CO_Checkout extends SetupClass {
 
 	}
 
-	@Then("^user is redirected to checkout page (\\d+)CO$")
+	/*@Then("^user is redirected to checkout page (\\d+)CO$")
 	public void user_is_redirected_to_checkout_page_CO(int arg1) throws Throwable {
 		Thread.sleep(6000);
     WebElement Continue =  driver.findElement(By.xpath("//*[@id='pg-checkout-shipping-info']/div[1]/div/div/div/div[2]/button"));
 	js.executeScript("arguments[0].scrollIntoView();",Continue);
 		Thread.sleep(1000);
 		Continue.click();
-	}
+	}*/
 
 	@Then("^user proceed to pay with (\\d+)CO (\\d+)CO$")
 	public void user_proceed_to_pay_with_CO_CO(int arg1, int arg2) throws InterruptedException {
 	     try {
 		Thread.sleep(1400);
-		// select 2co option
-		WebElement co_btn  = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='pg-payment-methods']/div[1]/div[1]/label/img")));
+		/*// select 2co option
+		WebElement co_btn  = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/div[4]/div/div[3]/div[1]/div[1]/form[1]/div/div[2]/label")));
 		js.executeScript("arguments[0].click();",co_btn);
 		     Thread.sleep(2000);
 	         co_btn.click();
@@ -174,7 +214,7 @@ public class CO_Checkout extends SetupClass {
 	     } catch( NoSuchElementException popup) { 
 	     }
 		
-		/*// place order button 
+		// place order button 
 		try {
 			
 		 WebElement place_order_btn  =  driver.findElement(By.cssSelector("#place-order-trigger > span"));
@@ -189,9 +229,33 @@ public class CO_Checkout extends SetupClass {
 		
 		
 	}*/
+		     try {
+		Thread.sleep(1400);
+		// select 2co option
+		WebElement co_btn  = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#pg-checkout-billing-payment-form > div > div:nth-child(2) > label")));
+		Thread.sleep(2000);
+	         co_btn.click();
+		Thread.sleep(5000);
+	     } catch( NoSuchElementException popup) { 
+	     }
+		
+		// place order button 
+		try {
+			
+		 WebElement place_order_btn  =  driver.findElement(By.cssSelector("body > div.afterBody.checkout-wrapper.main-wrapper.no-left-menu > div.main_wrapper > div > div.checkout-inner-wrapper > div.checkout-box-wrapper.checkout-order > div > div > table > tbody > tr:nth-child(4) > td:nth-child(1) > button.btn.primary-btn.pg-button.pg-checkout-continue"));
+			Thread.sleep(2000);
+			js.executeScript("arguments[0].scrollIntoView();",place_order_btn);	
+			//js.executeScript("arguments[0].click();", place_order_btn);
+			Thread.sleep(2000);
+		    place_order_btn.click();
+			Thread.sleep(5000);
+		} catch (NoSuchElementException popup) {
+		}
+		
+		
 	}
 
-	@Then("^paypal popup appears and user navigates back to my account (\\d+)CO$")
+	@Then("^2CO page appears and user navigates back to my account (\\d+)CO$")
 	public void paypal_popup_appears_and_user_navigates_back_to_my_account_CO(int arg1) throws Throwable {
 	    String co_page_title=driver.getTitle();
 		Thread.sleep(3000);
@@ -212,14 +276,14 @@ public class CO_Checkout extends SetupClass {
 		Thread.sleep(3000);
 	}
 
-	@Then("^user deleted the account (\\d+)CO$")
+	@Then("^user signout the account (\\d+)CO$")
 	public void user_deleted_the_account_CO(int arg1) throws Throwable {
 		Thread.sleep(3000);
-	    driver.get("https://www.slideteam.net/");
+	    driver.get("https://www.slidegeeks.com/");
 		Thread.sleep(2000);
 		
 
-		 driver.findElement(By.cssSelector("ul.header > li:nth-child(1) > a:nth-child(1)")).click();
+		/* driver.findElement(By.cssSelector("ul.header > li:nth-child(1) > a:nth-child(1)")).click();
 		 Thread.sleep(3000);
 		 
 		
@@ -261,8 +325,11 @@ public class CO_Checkout extends SetupClass {
 		js.executeScript("arguments[0].scrollIntoView();",continue_delete);
 		continue_delete.click();
 		 Thread.sleep(3000);
-	}
+	} */
+		
 
-
+       WebElement Signout = driver.findElement(By.xpath("//a[contains(text(),'Sign Out')]"));
+		Thread.sleep(3000);
+		Signout.click();
 	
 }
