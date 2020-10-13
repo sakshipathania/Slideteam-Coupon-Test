ddadac92ab445eb04be8a530dfee65228a091041
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-10-13 12:23:04 +0530

    Update CO_Checkout.java

--- src/test/java/SetupClass/StepDefinition/CO_Checkout.java
+++ src/test/java/SetupClass/StepDefinition/CO_Checkout.java
@@ -277,7 +277,7 @@ public class CO_Checkout extends SetupClass {
 	}
 
 	@Then("^user signout the account (\\d+)CO$")
-	public void user_deleted_the_account_CO(int arg1) throws Throwable {
+	public void user_signout_the_account_CO(int arg1) throws Throwable {
 		Thread.sleep(3000);
 	    driver.get("https://www.slidegeeks.com/");
 		Thread.sleep(2000);
