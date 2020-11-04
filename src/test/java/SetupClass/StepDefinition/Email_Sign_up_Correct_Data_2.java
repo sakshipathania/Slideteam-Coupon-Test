0ec09dc5fdd2873eab6786b43eab830af075b238
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-11-04 12:06:28 +0530

    Update Email_Sign_up_Correct_Data_2.java

--- src/test/java/SetupClass/StepDefinition/Email_Sign_up_Correct_Data_2.java
+++ src/test/java/SetupClass/StepDefinition/Email_Sign_up_Correct_Data_2.java
@@ -97,7 +97,7 @@ public class Email_Sign_up_Correct_Data_2 extends SetupClass {
 		driver.get("https://www.slidegeeks.com/business/product/roadmap-free-powerpoint-slide");
 		Thread.sleep(6000);
 		
-		WebElement Download= driver.findElement(By.cssSelector(".login-links > li:nth-child(2) > a:nth-child(1)"));
+		WebElement Download= driver.findElement(By.cssSelector("a.btn-download"));
 		Thread.sleep(3000);
 		js.executeScript("arguments[0].scrollIntoView();", Download);
 		 Download.click();
