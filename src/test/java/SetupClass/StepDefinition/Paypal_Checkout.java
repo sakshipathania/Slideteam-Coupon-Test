6e00585c88a8637709efde1a2c7075106f151539
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-12-07 17:07:54 +0530

    Update Paypal_Checkout.java

--- src/test/java/SetupClass/StepDefinition/Paypal_Checkout.java
+++ src/test/java/SetupClass/StepDefinition/Paypal_Checkout.java
@@ -2,6 +2,7 @@ package SetupClass.StepDefinition;
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
+import org.junit.Assert;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
