8500316d8f4fc65bfac44c3e507e4e31ac8f172d
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-12-07 14:57:12 +0530

    Update Paypal_Checkout.java

--- src/test/java/SetupClass/StepDefinition/Paypal_Checkout.java
+++ src/test/java/SetupClass/StepDefinition/Paypal_Checkout.java
@@ -129,7 +129,7 @@ public class Paypal_Checkout extends SetupClass {
 	}
 
 	@Then("^user is redirected checkout page$")
-	public void user_is_redirected_checkout_page(int arg1) throws Throwable {
+	public void user_is_redirected_checkout_page() throws Throwable {
 		Thread.sleep(6000);
     //WebElement Continue =  driver.findElement(By.xpath("//*[@id='pg-checkout-shipping-info']/div[1]/div/div/div/div[2]/button"));
 	//js.executeScript("arguments[0].scrollIntoView();",Continue);
