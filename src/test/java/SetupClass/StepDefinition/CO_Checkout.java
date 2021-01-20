99b7bac23ccf5aaf9c5d17c10da31db670fbf4e2
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2021-01-20 13:08:39 +0530

    Update CO_Checkout.java

--- src/test/java/SetupClass/StepDefinition/CO_Checkout.java
+++ src/test/java/SetupClass/StepDefinition/CO_Checkout.java
@@ -255,49 +255,6 @@ public class CO_Checkout extends SetupClass {
 		Thread.sleep(5000);
 	}
 	
-	@Then("^user is redirected to pricing page and check the text7 (\\d+)CO$")
-	public void user_is_redirected_to_pricing_page_and_check_the_text7_CO(int arg1) throws Throwable {
-		Thread.sleep(7000);
-		driver.get("https://www.slidegeeks.com/subscriptions");
-		       Thread.sleep(4000);
-		
-		Thread.sleep(3000);
-		String actualTitle = "Team License";
-		Thread.sleep(1000);
-		//wait.implictywait(driver);
-		String expectedTitle = "Team License";
-		Thread.sleep(1000);
-		//wait.implictywait(driver);
-		Assert.assertEquals(expectedTitle, actualTitle);
-		Thread.sleep(2000);
-		js.executeScript("alert('Text Team License is present and correct');");
-		Thread.sleep(3000);
-		driver.switchTo().alert().accept();
-		//wait.implictywait(driver);
-		Thread.sleep(2000);
-	}
-	
-	@Then("^user is redirected to pricing page and check the text8 (\\d+)CO$")
-	public void user_is_redirected_to_pricing_page_and_check_the_text8_CO(int arg1) throws Throwable {
-		Thread.sleep(7000);
-		driver.get("https://www.slidegeeks.com/subscriptions");
-		       Thread.sleep(4000);
-		
-		Thread.sleep(3000);
-		String actualTitle = "$599.99";
-		Thread.sleep(1000);
-		//wait.implictywait(driver);
-		String expectedTitle = "$599.99";
-		Thread.sleep(1000);
-		//wait.implictywait(driver);
-		Assert.assertEquals(expectedTitle, actualTitle);
-		Thread.sleep(2000);
-		js.executeScript("alert('Text $599.99 is present and correct');");
-		Thread.sleep(3000);
-		driver.switchTo().alert().accept();
-		//wait.implictywait(driver);
-		Thread.sleep(5000);
-	}
 	
 	@Then("^user is redirected to pricing page and check the text9 (\\d+)CO$")
 	public void user_is_redirected_to_pricing_page_and_check_the_text9_CO(int arg1) throws Throwable {
