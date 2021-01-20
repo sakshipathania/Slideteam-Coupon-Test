4ca2e53dcdab812b3824e0c013d551f15b23ad54
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2021-01-20 12:28:08 +0530

    Update CO_Checkout.java

--- src/test/java/SetupClass/StepDefinition/CO_Checkout.java
+++ src/test/java/SetupClass/StepDefinition/CO_Checkout.java
@@ -553,7 +553,7 @@ public class CO_Checkout extends SetupClass {
     @Then("^user is redirected to pricing page and choose a plan to pay (\\d+)CO$")
 	public void user_is_redirected_to_pricing_page_and_choose_a_plan_to_pay_CO(int arg1) throws Throwable {
 		// choose a plan
-		//driver.get("https://www.slidegeeks.com/subscriptions");
+		driver.get("https://www.slidegeeks.com/subscriptions");
 		Thread.sleep(3000);
 		//js.executeScript("window.scrollBy(0,1000)");
 		 WebElement Subscribe_btn  =  driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[2]/div[2]/div/div[1]/div/div[3]/div[3]/span/form/span/button"));
