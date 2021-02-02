78905ea78462951abb17f8344db5ea10d115ad44
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2021-02-02 16:00:59 +0530

    Update Paypal_Checkout.java

--- src/test/java/SetupClass/StepDefinition/Paypal_Checkout.java
+++ src/test/java/SetupClass/StepDefinition/Paypal_Checkout.java
@@ -671,7 +671,8 @@ public class Paypal_Checkout extends SetupClass {
 			    cancel_order_btn.click();
 				Thread.sleep(5000);*/
 		if(driver.findElement(By.xpath("/html/body/div[1]/section[2]/div/div/form/div[3]/div[1]/div[2]/div[1]/input")).isDisplayed())
-			 {WebElement PP_Email = driver.findElement(By.xpath("/html/body/div[1]/section[2]/div/div/form/div[3]/div[1]/div[2]/div[1]/input"))
+			 {
+			WebElement PP_Email = driver.findElement(By.xpath("/html/body/div[1]/section[2]/div/div/form/div[3]/div[1]/div[2]/div[1]/input"));
                          Thread.sleep(2000);
 		   PP_Email.sendkeys("tatvashardul-buyer@gmail.com");
 		   Thread.sleep(2000);
