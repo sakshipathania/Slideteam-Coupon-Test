1359a71d4a5ae3b0d2071d52dfccf058e46311f5
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2021-05-28 12:49:04 +0530

    Update Coupon_2.java

--- src/test/java/SetupClass/StepDefinition/Coupon_2.java
+++ src/test/java/SetupClass/StepDefinition/Coupon_2.java
@@ -18,34 +18,52 @@ import cucumber.api.java.en.Then;
 public class Coupon_2 extends SetupClass {
 	
 	WebDriverWait wait = new WebDriverWait(driver,50);
-         JavascriptExecutor js = (JavascriptExecutor) driver;
+	JavascriptExecutor js = (JavascriptExecutor) driver;
+
 	
-	@Given("^user is already on Website Home Page ii$")
-	public void user_is_already_on_Website_Home_Page_ii() throws Throwable {
+	@Given("^user is already on Website Home Page cc$")
+	public void user_is_already_on_Website_Home_Page_cc() throws Throwable {
 		driver.get(AppURL);
 		driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
 		log.info("It's opening the website URL");
-		Thread.sleep(1000);
-		
-	}
+	    Thread.sleep(2000);
+	    try {
+			WebElement logout = driver.findElement(By.xpath("//a[contains(text(),'Sign Out')]"));
+			if (logout.isEnabled()) {
+				logout.click();
+				Thread.sleep(8000);
+				driver.navigate().refresh();
+				Thread.sleep(2000);
+			}
+		} catch (NoSuchElementException Ext) {
 
-	@Then("^user navigates to sign up page ii$")
-	public void user_navigates_to_sign_up_page_ii() throws Throwable {
+		}
+	    Thread.sleep(3000);
+		try {
+			driver.findElement(By.cssSelector("ul.header > li:nth-child(1) > a:nth-child(1)")).click();
+			Thread.sleep(2000);
+			log.info("It's opening the website URL and redirect user to sign up page");
+		} 
+		catch (NoSuchElementException popup) {
+		}
 		
-		/* WebElement login_signup_btn = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".t3-navbar > ul:nth-child(1) > li:nth-child(10) > a:nth-child(1)")));
-		 Thread.sleep(3000);
-		 login_signup_btn.click();
-		 Thread.sleep(3000);*/
-		driver.get("https://www.slidegeeks.com/register");
-	
 		
-		 WebElement name = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#jform_name1")));
-		 Thread.sleep(3000);
-		 name.sendKeys("Automated Program");
-		 Thread.sleep(3000);
-	   
-	
-		// Generate Random Email Address
+		Thread.sleep(3000);
+	    driver.get("https://www.slidegeeks.com/subscriptions");
+		Thread.sleep(4000);
+		Thread.sleep(3000);
+		 WebElement Join_now = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#Monthly")));
+		Thread.sleep(3000);
+		Join_now.click();
+		Thread.sleep(5000);
+		
+		WebElement create_account = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#pg-account-action > div > div:nth-child(1) > div > div > div.btn-wrapper.create_accbtn > a")));
+		Thread.sleep(3000);
+		create_account.click();
+		Thread.sleep(2000);
+		//driver.get("https://www.slidegeeks.com/register");
+		Thread.sleep(3000);
+		
 		int leftLimit = 97; // letter 'a'
 	    int rightLimit = 122; // letter 'z'
 	    int targetStringLength = 10;
@@ -64,133 +82,201 @@ public class Coupon_2 extends SetupClass {
 	    String full_email="selenium.testing."+generatedString+"@gmail.com";
 	    System.out.println(full_email);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);	
+		//driver.findElement(By.id("email_address")).sendKeys(full_email);
+		
+
+		Thread.sleep(2000);
+	    WebElement new_email_signup = wait.until(ExpectedConditions.elementToBeClickable(By.id("email_address")));
+		Thread.sleep(2000);
+	    new_email_signup.sendKeys(full_email);
 		Thread.sleep(2000);
 		
-		WebElement new_email = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#jform_email1")));
-		 Thread.sleep(3000);
-		 new_email.sendKeys(full_email);
-		 Thread.sleep(3000);
-		
 	
-		WebElement password = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#jform_password1")));
-		 Thread.sleep(3000);
-		password.sendKeys("Geeks@123");
-		 Thread.sleep(3000);
-		 
-		 WebElement confirm_passwoed = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#jform_password2")));
-		 Thread.sleep(3000);
-		confirm_passwoed.sendKeys("Geeks@123");
-		 Thread.sleep(3000);
-	    
+		 WebElement new_fname_signup = wait.until(ExpectedConditions.elementToBeClickable(By.id("firstname")));
+		Thread.sleep(2000);
+	    new_fname_signup.sendKeys("Selenium");
+		Thread.sleep(2000);
+
+	    WebElement new_lname_signup = wait.until(ExpectedConditions.elementToBeClickable(By.id("lastname")));
+		Thread.sleep(2000);
+	    new_lname_signup.sendKeys("Testing");
+		Thread.sleep(2000);
 	
-		WebElement captcha = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#captchtext")));
-		 Thread.sleep(3000);
-		captcha.sendKeys("Y3Tt6bfwI");
-		 Thread.sleep(3000);
-	    
+		WebElement new_pwd_signup = wait.until(ExpectedConditions.elementToBeClickable(By.id("password")));
+			Thread.sleep(2000);
+		    new_pwd_signup.sendKeys("selenium@123");
+			Thread.sleep(2000);
 
-		WebElement register_btn = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".pg-register-button-new")));
-		 Thread.sleep(3000);
-		register_btn.click();
-		 Thread.sleep(5000);
+		    WebElement new_pwd1_signup = wait.until(ExpectedConditions.elementToBeClickable(By.id("password-confirmation")));
+			Thread.sleep(2000);
+		    new_pwd1_signup.sendKeys("selenium@123");
+			Thread.sleep(2000);
 		
-
-		driver.get("https://www.slidegeeks.com/free-downloads");
-		Thread.sleep(6000);
+		WebElement new_captcha_signup = wait.until(ExpectedConditions.elementToBeClickable(By.id("captcha_user_create")));
+		Thread.sleep(2000);
+	    new_captcha_signup.sendKeys("Aj7W2mtf9namwf55");
 		
-		WebElement Download= driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/section[2]/div/div/div[1]/div/div[2]/div[1]/div/div/p/a/span"));
-		
-		//WebElement Download = (WebElement)js.executeScript("('a.btn-download')", "");
-		Thread.sleep(3000);
-		js.executeScript("arguments[0].scrollIntoView();",Download);
-		Download.click();
-		Thread.sleep(3000);
-		
-		//Apply Coupon 
-		driver.get("https://www.slidegeeks.com/subscriptions");
+		WebElement new_btn_signup = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".submit")));
+		Thread.sleep(2000);
+	    new_btn_signup.click();
+		Thread.sleep(2000);
+	}
+	
+    
+    @Then("^user is redirected to pricing page and choose the plan to pay cc$")
+	public void user_is_redirected_to_pricing_page_and_choose_the_plan_to_pay_cc() throws Throwable {
+		// choose a plan
 		Thread.sleep(4000);
-		 WebElement Join_now = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#Monthly")));
+		driver.get("https://www.slideteam.net/pricing");
+		Thread.sleep(8000);
+		js.executeScript("window.scrollBy(0,1000)");
+		 WebElement join_now_btn  = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#Individual > div > div.slide-product > div:nth-child(2) > div.col.col2.col-bg2 > span > form > button > span")));
+			Thread.sleep(2000);
+		js.executeScript("arguments[0].scrollIntoView();",join_now_btn);
+		Thread.sleep(3300);
+		    join_now_btn.click();
+			Thread.sleep(6000);
+
+
+	}
+
+	@Then("^user is redirected checkout page cc$")
+	public void user_is_redirected_checkout_page_cc() throws Throwable {
+		Thread.sleep(6000);
+    //WebElement Continue =  driver.findElement(By.xpath("//*[@id='pg-checkout-shipping-info']/div[1]/div/div/div/div[2]/button"));
+	//js.executeScript("arguments[0].scrollIntoView();",Continue);
+		//Thread.sleep(1000);
+		//Continue.click();
+	}
+
+	@Then("^user proceed to pay with paypal pp cc$")
+	public void user_proceed_to_pay_with_paypal_pp_cc() throws Throwable {
+	
+		Thread.sleep(1400);
+		
+		   Thread.sleep(1400);
+		// select 2co option
+		WebElement co_btn  = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#amasty_stripe")));
+		Thread.sleep(2000);
+	         co_btn.click();
+		Thread.sleep(5000);
+	   
+		WebElement Stripe_name  = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#stripe-name")));
+		Thread.sleep(2000);
+	         Stripe_name.sendKeys("QA");
+		Thread.sleep(5000);
+		
+		
+		WebElement Stripe_email  = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#stripe-email")));
+		Thread.sleep(2000);
+	         Stripe_email.sendKeys("sakshi.pathania@slidetech.in");
+		Thread.sleep(5000);
+		
+		
+		WebElement Stripe_card  =  driver.findElement(By.className("InputElement is-empty Input Input--empty"));
+		Thread.sleep(2000);
+	         Stripe_card.sendKeys("4242424242424242");
+		Thread.sleep(5000);
+		   //  Stripe_card.clear();
+		  } catch( NoSuchElementException popup) { 
+	     }
+                Thread.sleep(5000);
 		Thread.sleep(3000);
-		Join_now.click();
-		Thread.sleep(3000);
-		 WebElement Coupon= driver.findElement(By.cssSelector("#discount-checkbox"));
-		Thread.sleep(3000);
-		Coupon.click();
-		Thread.sleep(3000);
-		 WebElement Add_Coupon= driver.findElement(By.xpath("/html/body/div[1]/div[4]/div/div[3]/div[1]/div[1]/form[2]/div[2]/div[2]/input"));
+		//Apply coupon
+
+		//WebElement Coupon= driver.findElement(By.cssSelector("#discount-code"));
+		//Thread.sleep(3000);
+		//Coupon.click();
+		//Thread.sleep(3000);
+		 WebElement Add_Coupon= driver.findElement(By.cssSelector("#discount-code"));
 		Thread.sleep(3000);
 		Add_Coupon.sendKeys("5OFF");
 		Thread.sleep(3000);
-		 WebElement Apply_Coupon= driver.findElement(By.cssSelector("#applyPromoCode"));
+		Thread.sleep(3000);
+		 WebElement Apply_Coupon= driver.findElement(By.cssSelector("#discount-form > div.actions-toolbar > div > button > span > span"));
 		Thread.sleep(3000);
 		Apply_Coupon.click();
 		Thread.sleep(3000);
+    
 		//Remove Coupon
-		 WebElement Remove_Coupon= driver.findElement(By.cssSelector("#removePromoCode"));
+		 WebElement Remove_Coupon= driver.findElement(By.cssSelector("#discount-form > div.actions-toolbar > div > button > span > span"));
 		Thread.sleep(3000);
 		Remove_Coupon.click();
 		Thread.sleep(3000);
+    
+    //Aplly Coupon Again
+		//WebElement Coupon1= driver.findElement(By.xpath("/html/body/div[1]/div[4]/div/div[3]/div[1]/div[1]/form[2]/div[2]/div[1]/label"));
+		//Thread.sleep(3000);
+		//Coupon1.click();
+		//Thread.sleep(3000);
 		
-		//Checkout
-		 WebElement place_order_btn  = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("body > div.afterBody.checkout-wrapper.main-wrapper.no-left-menu > div.main_wrapper > div > div.checkout-inner-wrapper > div.checkout-box-wrapper.checkout-order > div > div > table > tbody > tr:nth-child(4) > td:nth-child(1) > button.btn.primary-btn.pg-button.pg-checkout-continue")));
+  
+		
+		Thread.sleep(1000);
+		   WebElement place_order_btn  =  driver.findElement(By.cssSelector("#place-order-trigger > span"));
 			Thread.sleep(2000);
-			js.executeScript("arguments[0].scrollIntoView();",place_order_btn);	
+			js.executeScript("arguments[0].scrollIntoView();",place_order_btn);
 			//js.executeScript("arguments[0].click();", place_order_btn);
 			Thread.sleep(2000);
 		    place_order_btn.click();
 			Thread.sleep(5000);
+			
+			WebElement error_message= driver.findElement(By.cssSelector("#checkout > div.messages > div > div"));
+								     Thread.sleep(3000);
+			System.out.println("Please verify your Card details");
+								     Thread.sleep(2000);
+								  
+		} catch (NoSuchElementException popup) {
+		}
 		
-		 String stripe_page_title=driver.getTitle();
-		Thread.sleep(3000);
-	    System.out.println("Title of the Page is --> "+stripe_page_title);
-	    
-	    String page_title="https://checkout.stripe.com/";
-	    
-	    if(page_title.equalsIgnoreCase(stripe_page_title))
-	    {
-	    	System.out.println(" user is on the Stripe page");
-	    	log.info("USER IS ON THE STRIPE PAGE");
-	    }
-	    else
-	    {
-	    	System.out.println("user is on the wrong page");
-	    	log.info("USER IS ON THE WRONG PAGE");
-	    }	
+	}
+	@Then("^paypal popup appears and user navigates back to my account pp cc$")
+	public void paypal_popup_appears_and_user_navigates_back_to_my_account_pp_cc() throws Throwable {
+	         
+		// page title
+		//  String pp_page_title=driver.getTitle();
+		//	Thread.sleep(3000);
+		//    System.out.println("Title of the Page is --> "+pp_page_title);
 		
-		Thread.sleep(3000);
-		 WebElement Stripe_back = driver.findElement(By.cssSelector("#root > div > div > div.App-Overview > header > div > div > a > div > div > div.Header-backArrowContainer > svg"));
-		Thread.sleep(2000);
-			Stripe_back.click();
-		Thread.sleep(5000);
-					
-		//Signout
-		 //  driver.get("https://www.slidegeeks.com/component/pago/checkout");
 		//Thread.sleep(2000);
-	WebElement Account = driver.findElement(By.xpath("/html/body/div[1]/header/div/div/nav/div/div[2]/div[2]/div[2]/div/div[2]/ul/li[1]/a"));
-		Thread.sleep(3000);
-		Account.click();
-                 Thread.sleep(3000);
-	WebElement Delete_Account = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div/div/div[2]/div/ul/li[6]/a"));
-		Thread.sleep(3000);
-		js.executeScript("arguments[0].scrollIntoView();",Delete_Account);	
-		Thread.sleep(3000);
-		Delete_Account.click();
-                 Thread.sleep(3000);
-	WebElement Delete_Account_reason = driver.findElement(By.cssSelector("#only-free-download-product"));
-		Thread.sleep(3000);
-		Delete_Account_reason.click();
-                 Thread.sleep(3000);
-	WebElement Delete_Profile = driver.findElement(By.xpath("/html/body/div[1]/div[4]/div/div/div[3]/button[1]"));
-		Thread.sleep(3000);
-		Delete_Profile.click();
-                 Thread.sleep(3000);
-	WebElement No_Delete = driver.findElement(By.xpath("/html/body/div[1]/div[5]/div/div/div[3]/button[2]"));
-		Thread.sleep(3000);
-		No_Delete.click();
-                 Thread.sleep(3000);
-     //  WebElement Signout = driver.findElement(By.xpath("//a[@href ='/logout']"));
-	//	Thread.sleep(3000);
-		//Signout.click();
+				//  driver.get("https://www.slidegeeks.com/component/pago/checkout");
+				 // driver.switchTo().window(currentWindow);
+			 
 	}
 
+
+	@Then("^user Signout account PP cc$")
+	public void user_Signout_account_PP_cc() throws Throwable {
+		Thread.sleep(3000);
+
+		
+		 WebElement account = driver.findElement(By.xpath("//a[contains(.,'My Account')]"));
+			  Thread.sleep(3000);
+			account.click();
+		 Thread.sleep(3000);
+		 
+		
+
+		 WebElement delete_account = driver.findElement(By.cssSelector("#clicking"));
+		js.executeScript("arguments[0].scrollIntoView();",delete_account);
+		 delete_account.click();
+		 Thread.sleep(3000);
+		WebElement delete_reason = driver.findElement(By.cssSelector("#exampleRadios1"));
+		Thread.sleep(3000);
+		delete_reason.click();
+		Thread.sleep(3000);
+		
+		 WebElement delete_profile = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#delete-final")));
+		js.executeScript("arguments[0].scrollIntoView();",delete_profile);
+		delete_profile.click();
+		 Thread.sleep(3000);
+		
+		WebElement delete_profile_coupon = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#flipModal > div > div > div.modal-footer.button_action > button.btn.btn-default.button_2")));
+		js.executeScript("arguments[0].scrollIntoView();",delete_profile_coupon);
+		delete_profile_coupon.click();
+		 Thread.sleep(3000);
+	}
+
+
+	
 }
